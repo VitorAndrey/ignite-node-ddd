80b9d6375a9c49ec5e315829d97afb5f330d86c5
Vitor Andrey <vitor.andreylopes@gmail.com>
2024-08-27 19:43:39 -0300

    chore: add path alias for ts and for vitest

--- src/domain/use-cases/answer-question.test.ts
+++ src/domain/use-cases/answer-question.test.ts
@@ -1,7 +1,6 @@
-import { expect, test } from "vitest";
+import { Answer } from "../entities/answer";
 import { AnswerQuestionUseCase } from "./answer-question";
 import { AnswersRepository } from "../repositories/answers-repository";
-import { Answer } from "../entities/answer";
 
 const fakeRepository: AnswersRepository = {
   create: async (answer: Answer) => {
